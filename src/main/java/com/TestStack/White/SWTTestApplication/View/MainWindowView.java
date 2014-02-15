9aa25732e6092ef18d65e76a2218e3cd1e998949
murz42 <murz42@gmail.com>
2014-02-15 12:35:22 +0400

    all fixed

--- src/main/java/com/TestStack/White/SWTTestApplication/View/MainWindowView.java
+++ src/main/java/com/TestStack/White/SWTTestApplication/View/MainWindowView.java
@@ -18,14 +18,9 @@ public class MainWindowView {
 	public InputControlsTabView inputControlsTab;
 	
 	public MainWindowView()	{
-//        try {
-//		    display = new Display();
-//		    shell = new Shell(display);
-//        }
-//        catch (Exception ex) {
-            shell = new Shell();
-//        }
+        display = new Display();
 
+        shell = new Shell(display);
 		shell.setText("MainWindow");
 		shell.setLayout(mainLayout);
 
